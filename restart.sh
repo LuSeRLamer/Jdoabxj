133c11072c12fc23a1b35403ff2e98b0f7281f9f
root@lamer <84034984+LuSeRLamer@users.noreply.github.com>
2024-09-14 13:58:58 +0300

    restart.sh

---
+++ restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the script name
+SCRIPT_NAME="$0"
+
+# Get the current hour
+current_hour=$(date +%H)
+
+# Run the script every hour that has passed
+while true; do
+  sleep $(( 3600 - $(date +%s) % 3600 ))
+  echo "Restarting script at $current_hour:00"
+  exec $SCRIPT_NAME
+done
